e69439a5d8d4d8e444fe53af488b3d978c1be61b
Zeeshan-H <aashufta061@gmail.com>
2018-04-17 17:22:52 +0500

    ssaw

---
+++ signup_signin/Controllers/imageuploadController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using signup_signin.Models;
+using System.IO;
+
+namespace signup_signin.Controllers
+{
+    public class imageuploadController : Controller
+    {
+        // GET: imageupload
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult upload(ImageViewModel model)
+        {
+            tutorEntities et = new tutorEntities();
+            int imgId = 0;
+            var file = model.ImageFile;
+            byte[] imagebyte = null;
+            if (file != null)
+            {
+
+                file.SaveAs(Server.MapPath("/savedimages/" + file.FileName));
+                BinaryReader reader = new BinaryReader(file.InputStream);
+                imagebyte = reader.ReadBytes(file.ContentLength);
+                img img = new img();
+                img.Title = file.FileName;
+                img.imgByte = imagebyte;
+                img.Path = "/savedimages/" + file.FileName;
+                et.imgs.Add(img);
+                et.SaveChanges();
+                imgId = img.imgId;
+
+            }
+
+            return Json(imgId, JsonRequestBehavior.AllowGet);
+
+        }
+
+
+        public ActionResult display(int imgId)
+        {
+            tutorEntities et = new tutorEntities();
+
+            var img = et.imgs.SingleOrDefault(x => x.imgId == imgId);
+
+            return File(img.imgByte, "image/jpg");
+        }
+
+
+    }
+}
