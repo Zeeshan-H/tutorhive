fa67e3a6c510ea74334ffcc981ffd294a8cee056
Zeeshan-H <aashufta061@gmail.com>
2018-07-27 17:21:57 +0500

    Last commit

---
+++ signup_signin/Controllers/getingController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace signup_signin.Controllers
+{
+    public class getingController : Controller
+    {
+        // GET: geting
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Get(string id)
+        {
+            var dir = System.Web.HttpContext.Current.Server.MapPath("/UserImage");
+            var path = Path.Combine(dir, id + ".jpg");
+            return base.File(path, "image/jpeg");
+
+
+        }
+
+    }
+}
